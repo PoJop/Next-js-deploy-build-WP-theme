e00d55b4360edeaec312f3f8ce6348cfed5992ed
Oleg <kostetzkiy.oleg@gmail.com>
2022-01-06 11:06:32 +0200

    commit

--- scripts.js
+++ scripts.js
@@ -1,12 +1,43 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
+
+    let buildFilePage = {
+        page: [],
+        buildCode: '',
+    }
+    document.querySelector('.zip').addEventListener('click', async () => {
+        await axios({
+            method: 'post',
+            url: 'https://server-utils-wp-nextjs.herokuapp.com/api/zip/',
+            responseType: 'arraybuffer',
+            data: buildFilePage,
+        })
+            .then(response => {
+                let blob = new Blob([response.data], { type: 'application/zip' })
+
+                const link = document.createElement('a');
+                if (link.download !== undefined) {
+                    const url = URL.createObjectURL(blob);
+                    link.setAttribute('href', url);
+                    link.setAttribute('download', buildFilePage.buildCode);
+                    link.style.visibility = 'hidden';
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+                }
+            });
+    })
+
+
     let json = []
     const root = document.querySelector('.root')
     const css = document.querySelector('.css')
     const js = document.querySelector('.js')
     let nextCode = ''
     let path = ''
-    let copyJS =''
-    let copyCSS =''
+    let copyJS = ''
+    let copyCSS = ''
 
     document.querySelector('.nextConeForm').onsubmit = async (e) => {
         e.preventDefault();
@@ -19,13 +50,14 @@ document.addEventListener('DOMContentLoaded', () => {
         json = [JSON.parse(e.target[0].value.replace(/\s{2,}/g, '').replace(/\r?\n/g, ""))]
         validate()
     };
-    
-    document.querySelector('.copy_all').onclick = () => {navigator.clipboard.writeText(copyCSS + copyJS)}
-    document.querySelector('.copy_js').onclick = () => {navigator.clipboard.writeText(copyJS)}
-    document.querySelector('.copy_css').onclick = () => {navigator.clipboard.writeText(copyCSS)}
-    
+
+    document.querySelector('.copy_all').onclick = () => { navigator.clipboard.writeText(copyCSS + copyJS) }
+    document.querySelector('.copy_js').onclick = () => { navigator.clipboard.writeText(copyJS) }
+    document.querySelector('.copy_css').onclick = () => { navigator.clipboard.writeText(copyCSS) }
+
     const validate = () => {
-        
+        buildFilePage.page = []
+        buildFilePage.buildCode = ''
         copyJS = ''
         copyCSS = ''
         const state = {
@@ -37,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
-        if(json[0] === undefined) return false;
+        if (json[0] === undefined) return false;
         for (let i = 0; i < Object.keys(json[0]).length; i++) {
 
             if (Object.keys(json[0])[i] === 'polyfillFiles') {
@@ -53,6 +85,8 @@ document.addEventListener('DOMContentLoaded', () => {
             if (Object.keys(json[0])[i] === 'lowPriorityFiles') {
                 let lowPriorityFiles = json[0]['lowPriorityFiles']
 
+                buildFilePage.buildCode = nextCode.length > 0 ? nextCode : lowPriorityFiles[0].substring(7, lowPriorityFiles[0].indexOf('/', 7))
+            
                 lowPriorityFiles.forEach((e) => {
                     let firstSlash = e.indexOf('/', 0)
                     let secondSlash = e.indexOf('/', ++firstSlash)
@@ -69,8 +103,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 for (let j = 0; j < Object.keys(pages).length; j++) {
 
                     pages[`${Object.keys(pages)[j]}`].forEach((page) => {
-                        if (page.includes('.js')) state.pages.pagesJS.push(`
-                    <script src='/_next/${page}' defer=''></script>`)
+                        if (page.includes('.js')) {
+                            state.pages.pagesJS.push(`
+                            <script src='/_next/${page}' defer=''></script>`)
+                            if (!page.includes('_') && page.includes('pages')) {
+                                let path = 'pages/'
+                                let f = page.indexOf(path, 0)
+                                let s = page.indexOf("-", f)
+                                console.log(f, s)
+                                console.log(page.substring(f + path.length, s))
+                                // console.log(page[f + path.length],page[f + path.length],page[f + path.length],page[f + path.length])
+                                buildFilePage.page.push(page.substring(f + path.length, s))
+                            }
+                        }
                         if (page.includes('.css')) state.pages.pagesCSS.push(`
                     <link rel='preload' href='/_next/${page}' as='style' />
                     <link rel='stylesheet' href='/_next/${page}' data-n-g='' />
@@ -92,6 +137,7 @@ document.addEventListener('DOMContentLoaded', () => {
         css.innerText = `${arrCSS.replace(/,/g, " ")}`
         copyJS = js.innerText
         copyCSS = css.innerText
+        console.log(buildFilePage)
     }
     validate()
 })
